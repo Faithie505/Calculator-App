1d3cdc7f64723a463b23db289d62f67eccb43e04
Fathia Elias <faitholami16@gmail.com>
2024-10-09 20:32:53 +0100

    created clear() function to clear result screen when clicked

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-//variables:
+document.getElementById("b18").addEventListener("click", cleared);
 
+function cleared()
+{
+   document.getElementById("result").value = "";
+    document.getElementById("result").innerHTML = "";
+}
 
 //displays the calculations on the screen
 function display(val)
@@ -9,12 +14,18 @@ function display(val)
     
 }
 
-
-
+//does the caalculation of the answer and displays it on the screen when equals is clicked
 function solve()
 {
     let x = document.getElementById("result").value;
     let y = eval(x);
+    let equation = document.getElementById("result").value;
     document.getElementById("result").value = y;
     document.getElementById("result").innerHTML = y;
+
+   document.getElementById("prevCalc").innerHTML = equation+" = "+ y;
 }
+
+
+
+
